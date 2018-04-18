6286739bfa03b30c1a4bb96befc7ce5c941762b5
Jia Liu <jia.liu@maxent-inc.com>
2018-04-18 17:39:02 +0800

    first commit

---
+++ src/main/java/org/liujia/randomGeoIP/Boot.java
@@ -0,0 +1,109 @@
+package org.liujia.randomGeoIP;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.maxmind.geoip2.DatabaseReader;
+import com.maxmind.geoip2.exception.GeoIp2Exception;
+import com.maxmind.geoip2.model.CityResponse;
+import com.maxmind.geoip2.record.Location;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by liujia on 2018/4/18.
+ * Generate Chinese random ip and geo.
+ * ref: https://github.com/taliu/chinese-random-ip
+ *      https://www.maxmind.com/zh/geoip-demo
+ */
+public class Boot {
+
+    private Random random = new Random();
+    private static DatabaseReader geoDB;
+    private static JSONObject ipDB;
+
+    private static void init() throws IOException, URISyntaxException {
+        initIPDB();
+        initGeoDB();
+    }
+
+    private static void initIPDB() throws URISyntaxException, IOException {
+        StringBuilder sb = new StringBuilder();
+        List<String> lines;
+        lines = Files.readAllLines(Paths.get(ClassLoader.getSystemResource("ChineseIP.json").toURI()));
+        lines.stream().forEachOrdered(sb::append);
+        ipDB = JSON.parseObject(sb.toString());
+    }
+
+    private static void initGeoDB() throws IOException, URISyntaxException {
+        File database = new File(String.valueOf(Paths.get(ClassLoader.getSystemResource("GeoLite2-City.mmdb").toURI())));
+        geoDB = new DatabaseReader.Builder(database).build();
+    }
+
+
+    private String getRandomChineseIP() throws IOException {
+        List<String> provinces = new ArrayList<>(ipDB.keySet());
+        int i = random.nextInt(provinces.size());
+        String province = provinces.get(i);
+
+        JSONArray ranges = ipDB.getJSONArray(province);
+        int i1 = random.nextInt(ranges.size());
+        JSONObject jsonObject = ranges.getJSONObject(i1);
+        Long min = ipToLong(jsonObject.getString("min"));
+        Long max = ipToLong(jsonObject.getString("max"));
+
+        // data accuracy loss, maybe a bug
+        long l = random.nextInt((int) (max - min + 1)) + min;
+        return longToIp(l);
+    }
+
+
+    private String getGeoForIP(String ip) throws IOException, GeoIp2Exception {
+        InetAddress ipAddress = InetAddress.getByName(ip);
+        CityResponse response = geoDB.city(ipAddress);
+        Location location = response.getLocation();
+        return location.getLatitude() + "," + location.getLongitude();
+    }
+
+    private static String longToIp(long ip) {
+        long ip4 = ip & 0xFF;
+        long ip3 = (ip >> 8) & 0xFF;
+        long ip2 = (ip >> 16) & 0xFF;
+        long ip1 = (ip >> 24) & 0xFF;
+        return String.format("%d.%d.%d.%d", ip1, ip2, ip3, ip4);
+    }
+
+    private static Long ipToLong(String ip) {
+        if (ip == null || ip.equals("")) {
+            return -1L;
+        }
+        Long ips = 0L;
+        String[] numbers = ip.split("\\.");
+
+        for (int i = 0; i < 4; ++i) {
+            ips = ips << 8 | Integer.parseInt(numbers[i]);
+        }
+        return ips;
+    }
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        Boot boot = new Boot();
+        init();
+        try {
+            String randomChineseIP = boot.getRandomChineseIP();
+            System.out.println(randomChineseIP);
+            String geoForIP = boot.getGeoForIP(randomChineseIP);
+            System.out.println(geoForIP);
+        } catch (IOException | GeoIp2Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
